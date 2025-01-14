cd06da0a7121020df5c06334531e770a09b255c1
Nolan Welch <nolanwelch@outlook.com>
2025-01-13 21:06:01 -0500

    update install script to use variable for package name and improve alias definition

--- ai-revise/install.sh
+++ ai-revise/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-CFG_FILE=~/.bashrc
-PACKAGE_ALIAS='alias airevise="python -m airevise"'
+PACKAGE_NAME=airevise
 
-python -m pip install -r airevise/requirements.txt
-python -m pip install airevise/
+CFG_FILE=~/.bashrc
+PACKAGE_ALIAS="alias $PACKAGE_NAME=\"python -m $PACKAGE_NAME\""
+
+python -m pip install -r "$PACKAGE_NAME/requirements.txt"
+python -m pip install "$PACKAGE_NAME/"
 
 if ! grep -q "$PACKAGE_ALIAS" "$CFG_FILE"; then
     echo "$PACKAGE_ALIAS" >>"$CFG_FILE"
